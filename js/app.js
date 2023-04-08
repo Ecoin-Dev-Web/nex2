a3d89c8ec45e3f48878851e91e68524407a5aa90
moh-hmd <hamdadotmane@gmail.com>
2023-04-08 11:57:31 +0100

    commit

--- js/app.js
+++ js/app.js
@@ -1,1 +1,32 @@
-confirm ("5555")
+// var num = 25
+// var note = 16.25
+// var coef = 8
+
+// console.log(typeof num)
+// console.log(num)
+
+// console.log(typeof note)
+// console.log(note)
+
+// console.log(typeof coef)
+// console.log(coef)
+
+var list1 = [10,11,12,13,14]
+var list2 = []
+ list2[0] = 10
+ list2[1] = 11
+ list2[2] = 12
+ list2[3] = 13
+ list2[4] = 14
+
+ var list3 = new Array(15,16,17,18,19)
+ var list4 = new Array()
+ list4[0] = 15
+ list4[1] = 16
+ list4[2] = 17
+ list4[3] = 18
+ list4[4] = 19
+ console.log(list1)
+ console.log(list2)
+ console.log(list3)
+ console.log(list4)
